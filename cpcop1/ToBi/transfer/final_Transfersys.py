9bdbbc0542ef83f4dc53ad296526dfd291fd3862
rlaxodud214 <rlaxodud214@naver.com>
2020-11-27 00:47:12 +0900

    11-27 ver2

--- cpcop1/ToBi/transfer/final_Transfersys.py
+++ cpcop1/ToBi/transfer/final_Transfersys.py
@@ -1,22 +1,84 @@
 from urllib.request import urlopen
-from urllib.parse import quote_plus, unquote
+from urllib.parse import urlencode, quote_plus, unquote
 from bs4 import BeautifulSoup
 from selenium import webdriver
 # location.py 파일 필요 -> 딕셔너리의 키는 역이름이고 값은 좌표를 가진 데이터 파일임
-import xmltodict, json,location
+import xmltodict, json, urllib, location, requests, time, tabula
 import pandas as pd
+import hashlib
+import hmac
+import base64
 import requests
 import time
+import numpy as np
 from urllib.parse import urlencode
-from urllib.request import urlopen
+from urllib.request import urlopen, Request
 import urllib
-import dict_go as dict_go
+import bs4
+import dict_go
 # dict_go.py 있어야함 -> 딕셔너리의 키는 시간, 값은 셔틀 분 데이터 값
 
 
+def IPCall():
+    enc_location = urllib.parse.quote("ip 주소 확인")
+    url = 'https://search.naver.com/search.naver?ie=utf8&query=' + enc_location
+    req = Request(url)
+    page = urlopen(req)
+    html = page.read()
+    soup = bs4.BeautifulSoup(html, 'html5lib')
+    ip = soup.select("div[class=ip_chk_box]")
+    name = str(ip[0])
+    answer = name[24:]
+    array = answer.split("<")
+    return array[0]
 
 
-# hour = 시간표 시간, Startx = 시작위치.....End
+ Signature Make
+def make_signature(method, basestring, timestamp, access_key, secret_key):
+    message = method + " " + basestring + "\n" + timestamp + "\n" + access_key
+    message = bytes(message, 'utf-8')
+    signature = base64.b64encode(hmac.new(secret_key, message, digestmod=hashlib.sha256).digest())
+
+    return signature
+
+
+def requestApi(timestamp, access_key, signature, uri):
+    # Header for Request
+    headers = {'x-ncp-apigw-timestamp': timestamp,
+               'x-ncp-iam-access-key': access_key,
+               'x-ncp-apigw-signature-v2': signature}
+
+    # Geolocation API Request
+    res = requests.get(uri, headers=headers)
+
+    # Check Response
+    content = res.content.decode(encoding='utf-8')
+    content1 = content.split(",")
+    content1 = np.array(content1)
+    content1[7] = content1[7].replace('"lat": ', '')
+    content1[8] = content1[8].replace('"long": ', '')
+    return content1[7], content1[8]
+
+
+def Now_LatNLon():
+    method = "GET"
+    ipname = IPCall()
+    basestring = "/geolocation/v2/geoLocation?ip=" + ipname + "&ext=t&responseFormatType=json"
+    timestamp = str(int(time.time() * 1000))
+    access_key = "1bl8GtXyTTzXDCG4lgrA"  # access key id (from portal or sub account)
+    secret_key = "C43S00RfBboTCIm89TlO8DxqLzChvuLBgr0fO2SD"  # secret key (from portal or sub account)
+    secret_key = bytes(secret_key, 'UTF-8')
+    signature = make_signature(method, basestring, timestamp, access_key, secret_key)
+
+    # GET Request
+    hostname = "https://geolocation.apigw.ntruss.com"
+    requestUri = hostname + basestring
+    (lat, lon) = requestApi(timestamp, access_key, signature, requestUri)
+
+    return (lat, lon)
+
+
+ hour = 시간표 시간, Startx = 시작위치.....End
 def Span_Time(Near_Station_Lat, Near_Station_Lon):
     url = 'http://ws.bus.go.kr/api/rest/pathinfo/getPathInfoBySubway'
     params = '?' + urlencode({
@@ -120,7 +182,7 @@ def eclass(id, password):
     options.add_argument('disable-gpu')
 
     # 위치 지정
-    driver = webdriver.Chrome("/home/bitnami/cpcop/cpcop1/ToBi/transfer/chromedriver", options=options)
+    driver = webdriver.Chrome("C:\\ttemp/chromedriver.exe", options=options)
 
     # 웹 자원 로드를 위해 암묵적으로 딜레이
     delay_time = 3
@@ -216,42 +278,55 @@ def Sub_Time(hour, minute, inja):
     m = cm % 60
     return h, m
 
-def main():
-    start = time.time()
-    dictgo = dict_go.dict_time_table_go
-    now_location = ['37.342261', '126.72805869999999']
-    # now_location = Now_LatNLon()
-    nearsub1 = Near_Sub_Station(now_location[0], now_location[1])  # 좌표(현재 위치) -> 정왕역 (좌표로 부터 가까운 역 검색)
-    index1 = nearsub1.rstrip('역')  # 정왕역 -> 정왕
-    # n = time.localtime().tm_wday
-    n = 1
-    weeks = ['월', '화', '수', '목', '금']
-    # print(weeks[n])
-    # data = int(eclass("2015162013", 'mouse3178!')[weeks[n]])
-    data = eclass("2019152025", 'su9191su?k')[weeks[n]]
-    h1 = int(data['hour'])
-    m1 = int(data['minute'])
-    std = Shuttle_Time_go(h1, m1, dictgo)
-    h2 = int(std[0])
-    m2 = int(std[1])
-    dict1 = {}
-    dict1['starty'], dict1['startx'] = location.dict_location[index1]  # 정왕 -> 정왕역 좌표 [37.351735, 126.742989] (역이름으로 좌표 반환)
-    dict1['starty'], dict1['startx'] = "37.497969", "127.026654"
-    time_info = Span_Time(dict1['startx'], dict1['starty'])
-    h3, m3 = Sub_Time(h2, m2, int(time_info))
-    Travel_time_bus = Near_Bus_Station(now_location[0], now_location[1])
-    h4, m4 = Sub_Time(h3, m3, Travel_time_bus)
-    h5, m5 = Sub_Time(h4, m4, 10)
-    print("출발 시스템"+"-"*60)
-    print("현재 " + str(now_location) + "의 위치에서 가장 가까운 역은 " + str(index1) + "역입니다.\n")
-    print("출발 추천시간 (집-> 버스정류장) : " + str(h5) + "시 " + str(m5) + "분(예상소요시간 : 10분)\n")
-    if Travel_time_bus > 4:
-        print("버스정류장->%s역 : %s시 %s분(예상소요시간 : %s분)" % (index1, str(h4), str(m4), str(Travel_time_bus)))
-    print(nearsub1 + "에서 출발해야하는 시간 : " + str(h3) + "시 " + str(m3) + "분(예상소요시간 : " + time_info + "분)")
-    print("정왕역에서 출발하는 " + str(h2) + "시 " + str(m2) + "분 셔틀 탑승")
-    print("학교에 도착해야하는 시간 : %d시 %d분 (시간표 정보 읽어오기)\n" % (h1, m1))
-    rjfflstlrks = time.time() - start
-    print('응답시간 = '+str(rjfflstlrks))
+start = time.time()
+dictgo = dict_go.dict_time_table_go
+now_location = ['37.491575', '127.030912']
+ now_location = Now_LatNLon()
+nearsub1 = Near_Sub_Station(now_location[0], now_location[1])  # 좌표(현재 위치) -> 정왕역 (좌표로 부터 가까운 역 검색)
+rjfflstlrks = time.time() - start
+print('응답시간 = '+str(rjfflstlrks))
+index1 = nearsub1.rstrip('역')  # 정왕역 -> 정왕
+ n = time.localtime().tm_wday
+n = 1
+weeks = ['월', '화', '수', '목', '금']
+ print(weeks[n])
+ data = int(eclass("2015162013", 'mouse3178!')[weeks[n]])
+data = eclass("2019152025", 'su9191su?k')[weeks[n]]
+h1 = int(data['hour'])
+m1 = int(data['minute'])
+rjfflstlrks = time.time() - start
+print('응답시간 = '+str(rjfflstlrks))
+temp = rjfflstlrks
 
-if __name__ == '__main__':
-    main()
+std = Shuttle_Time_go(h1, m1, dictgo)
+h2 = int(std[0])
+m2 = int(std[1])
+rjfflstlrks = time.time() - start
+print('응답시간 = '+str(rjfflstlrks-temp))
+temp = rjfflstlrks
+
+dict1 = {}
+dict1['starty'], dict1['startx'] = location.dict_location[index1]  # 정왕 -> 정왕역 좌표 [37.351735, 126.742989] (역이름으로 좌표 반환)
+time_info = Span_Time(dict1['startx'], dict1['starty'])
+h3, m3 = Sub_Time(h2, m2, int(time_info))
+rjfflstlrks = time.time() - start
+print('응답시간 = '+str(rjfflstlrks-temp))
+temp = rjfflstlrks
+
+Travel_time_bus = Near_Bus_Station(now_location[0], now_location[1])
+h4, m4 = Sub_Time(h3, m3, Travel_time_bus)
+rjfflstlrks = time.time() - start
+print('응답시간 = '+str(rjfflstlrks-temp))
+temp = rjfflstlrks
+
+h5, m5 = Sub_Time(h4, m4, 10)
+print("출발 시스템"+"-"*60)
+print("현재 " + str(now_location) + "의 위치에서 가장 가까운 역은 " + str(index1) + "역입니다.\n")
+print("출발 추천시간 (집-> 버스정류장) : " + str(h5) + "시 " + str(m5) + "분(예상소요시간 : 10분)\n")
+if Travel_time_bus > 4:
+    print("버스정류장->%s역 : %s시 %s분(예상소요시간 : %s분)" % (index1, str(h4), str(m4), str(Travel_time_bus)))
+print(nearsub1 + "에서 출발해야하는 시간 : " + str(h3) + "시 " + str(m3) + "분(예상소요시간 : " + time_info + "분)")
+print("정왕역에서 출발하는 " + str(h2) + "시 " + str(m2) + "분 셔틀 탑승")
+print("학교에 도착해야하는 시간 : %d시 %d분 (시간표 정보 읽어오기)\n" % (h1, m1))
+rjfflstlrks = time.time() - start
+print('응답시간 = '+str(rjfflstlrks))
